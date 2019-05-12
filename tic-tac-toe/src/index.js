32885029b01408b9fe0ce388d00b1a1d010bd300
iperea <iperea@aquatec.es>
2019-05-12 10:40:27 +0200

    Refactorizado para mostrar tablero usando un bucle en lugar de hacerlo hardcodeado.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,58 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Board extends React.Component {
-    /*
-        constructor(props) {
-            super(props);
-            this.state = {
-                squares: Array(9).fill(null),
-                xIsNext: true,
-            }
-        }
-    */
+
     renderSquare(i) {
         return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
     }
 
     render() {
-
+        var lista = [];
+        for (var i = 0; i < 3; i++) {
+            var sublista = [];
+            for (var j = 0; j < 3; j++) {
+                sublista.push(this.renderSquare(j * 3 + i));
+            }
+            lista.push(
+                <div className="board-row">{sublista}</div>
+            )
+        }
         return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
+            <div>{lista}</div>
         );
     }
 }
 
-/**
-class Square extends React.Component {
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
-}
-**/
 
 function Square(props) {
     return (
